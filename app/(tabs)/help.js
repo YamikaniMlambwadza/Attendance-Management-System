c920f18e9c5d729ca992d90f4a35fbdcc1bb5e40
YamikaniMlambwadza <bed-com-61-22@unima.ac.mw>
2024-11-27 02:31:49 +0200

    second commit

--- app/(tabs)/help.js
+++ app/(tabs)/help.js
@@ -1,12 +1,24 @@
+
 import { Text, View } from 'react-native';
 import React from 'react';
 
 export default function Help() {
   return (
     <View className="items-center h-full pt-10 bg-white">
-      <Text className="text-black text-lg font-bold">
+      <Text className="text-black text-lg font-bold mb-6">
         help and support
       </Text>
+      <View className="px-6">
+        <Text className="text-black text-base mb-4 text-left">
+          1. Register a student
+        </Text>
+        <Text className="text-black text-base mb-4 text-left">
+          2. Check records
+        </Text>
+        <Text className="text-black text-base mb-4 text-left">
+          3. Frequently Asked Questions (FAQs)
+        </Text>
+      </View>
     </View>
   );
 }
