0590d0b4ce5ca95dba4178d526387b2ada4a8e00
YamikaniMlambwadza <bed-com-61-22@unima.ac.mw>
2024-11-30 14:02:30 +0200

    six commit

--- app/(tabs)/help.js
+++ app/(tabs)/help.js
@@ -70,6 +70,18 @@ export default function Help() {
         <Text classNme="text-black text-base mb-4 text-left">
           3. Frequently Asked Questions (FAQs)
         </Text>
+        <text classnam="text-black text-base mb-4 text-left ml-8">
+          General Usage
+        </text>
+        <Text classname="text-black text-base mb-4 text-left ml-12">
+         i.How can i scan student id to mark attendance 
+        </Text>
+        <Text classname="text-black text-base mb-4 text-left ml-12">
+          ii. How can i scan student id to mark submission
+        </Text>
+        <Text classname="text-black text-base mb-4 text-left ml-12">
+          iii. can i scan multiple student id silmutaneously or does it need to be one at a time
+        </Text>
       </View>
     </View>
   );
